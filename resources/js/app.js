37da2475c35d939e127f257bb68cd7fb7ddf0b10
Alejandro <alejandrojsrv@gmail.com>
2019-06-13 19:29:51 -0500

    a

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,13 +12,19 @@ Vue.mixin({
         }
     }
 })
+import axios from 'axios';
+let user =  document.head.querySelector();
+localStorage.setItem(JSON.stringify());
+axios.defaults.headers.common['Authorization'] = 'Bearer '+localStorage.getItem('auth-token');
+import Multiselect from 'vue-multiselect'
+Vue.component('multiselect', Multiselect);
+import 'vue-multiselect/dist/vue-multiselect.min.css';
 import Vuetify from 'vuetify'
 import VueNoty from 'vuejs-noty'
 import 'vuetify/src/stylus/app.styl';
 import 'vuejs-noty/dist/vuejs-noty.css'
 import VuetifyConfirm from 'vuetify-confirm';
-import vSelect from 'vue-select'
-Vue.component('v-select', vSelect)
+
 Vue.use(Vuetify)
 Vue.use(VueNoty, {
     timeout: 3500,
